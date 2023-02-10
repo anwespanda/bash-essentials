57d3c1cf036d17d52f08b93e683394be0ca50614
anwespanda <mail@anwespanda.com>
2023-02-11 00:25:47 +0530

    Fixed broken grep command when the name contained quality like 1080p. Also fixed leading and trailing spaces.

--- changeName.sh
+++ changeName.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Define the directory where the movie folders are located
 directory="./test"
@@ -10,22 +11,22 @@ do
   if [ -d "$file" ]; then
     # Get the original folder name
     original_name=$(basename "$file")
-    echo $original_name
 
     # Remove all characters that are not letters or numbers
     cleaned_name=$(echo "$original_name" | tr -cd '[:alnum:] \.' )
-
+    
     # Extract the year from the cleaned name
-    year=$(echo "$cleaned_name" | grep -oE '[0-9]{4}')
+    year=$(echo "$cleaned_name" | grep -oE '19[0-9]{2}|20[0-9]{2}')
 
     # Remove everything after the year
-    movie_name=$(echo "$cleaned_name" | sed "s/$year.*//")
+    movie_name=$(echo "$cleaned_name" | sed "s/\($year\).*//")
+
+    # Replace dots with spaces
+    movie_name=$(echo "$movie_name" | sed -e 's/\./ /g')
 
     # Remove any leading or trailing spaces from the movie name
     movie_name=$(echo "$movie_name" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
-    # Replace dots with spaces
-    movie_name=$(echo "$movie_name" | sed -e "s/\./ /g")
     # Create the final renamed folder name
     renamed_folder="$movie_name ($year)"
 
